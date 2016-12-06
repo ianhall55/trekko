d9b00093dda2cf8f6fc43f497efb6232cdde54d2
Ian Hall <halli2008@yahoo.com>
2016-12-06 15:10:18 -0800

    added trips to database

--- db/migrate/20161130221459_create_trips.rb
+++ db/migrate/20161130221459_create_trips.rb
@@ -1,7 +1,9 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
-
+      t.string :country, null: false
+      t.integer :user_id, null: false
+      
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109215330) do
+ActiveRecord::Schema.define(version: 20161130221459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "country",    null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
