2c13e6ca95ce50b0dccd583cfa6df04c928fe66a
Ian Hall <halli2008@yahoo.com>
2017-02-03 15:19:03 -0700

    changed oauth middleware

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
-    scope: 'email,name,first_name,last_name,location',
-    display: 'popup',
-    secure_image_url: true
+  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']
 end
+
+# scope: 'email,name,first_name,last_name,location',
+# secure_image_url: true
